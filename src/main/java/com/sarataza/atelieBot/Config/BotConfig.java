bc30690c4080cd54369b02563028095c18dcee08
Kirill Shilov <sarataza@inbox.ru>
2025-04-10 15:50:35 +0300

    проверка автоматики

--- src/main/java/com/sarataza/atelieBot/Config/BotConfig.java
+++ src/main/java/com/sarataza/atelieBot/Config/BotConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+
 @Data
 @PropertySource("application.properties")
 public class BotConfig {
